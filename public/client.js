96278812bffd255648de29e6b4d6cbd083178759
Casey Johnson <johnson.casey1211@gmail.com>
2022-11-06 22:11:16 -0600

    chore: saving work

--- public/client.js
+++ public/client.js
@@ -1,5 +1,6 @@
-const localVideo = document.getElementById('local-video');
+const cameraVideo = document.getElementById('camera-video');
 const peers = {};
+const screenShareButton = document.getElementById('screen-share-button');
 const servers = {
 	iceServers: [
 		{
@@ -13,90 +14,168 @@ const servers = {
 	],
 };
 const socket = io();
+const videos = document.getElementById('videos');
 
-socket.connect('/abc');
-socket.emit('join room', 'abc');
+const createPeer = (userID, source) => {
+	peers[userID] = peers[userID] ?? {};
+	peers[userID][source] = new RTCPeerConnection(servers);
 
-const localStream = (async () => {
-	return await navigator
+	peers[userID][source].onicecandidate = (rtcPeerConnectionIceEvent) => {
+		if (rtcPeerConnectionIceEvent.candidate) {
+			socket.emit(
+				'ice-candidate',
+				{
+					candidate: rtcPeerConnectionIceEvent.candidate,
+					source,
+					target: userID,
+				},
+			);
+		}
+	};
+
+	peers[userID][source].onnegotiationneeded = () => {
+		peers[userID][source].createOffer()
+			.then((offer) => peers[userID][source].setLocalDescription(offer))
+			.then(() => {
+				socket.emit(
+					'offer',
+					{
+						sdp: peers[userID][source].localDescription,
+						source,
+						target: userID,
+					},
+				);
+			})
+			.catch((error) => console.log(error));
+	};
+
+	peers[userID][source].ontrack = (rtcTrackEvent) => {
+		if (rtcTrackEvent.track.kind === 'video') {
+			const newVideo = document.createElement('video');
+			newVideo.setAttribute('autoplay', 'true');
+			newVideo.setAttribute('controls', 'true');
+			newVideo.classList.add(userID, source);
+			newVideo.setAttribute('playsinline', 'true');
+			newVideo.srcObject = rtcTrackEvent.streams[0];
+			const newVideoContainer = document.createElement('div');
+			newVideoContainer.classList.add('video-container');
+			newVideoContainer.appendChild(newVideo);
+			const existingVideos = document.getElementsByClassName(userID);
+	
+			if (existingVideos.length > 1) {
+				existingVideos[existingVideos.length - 2].parentElement.after(newVideoContainer);
+			} else {
+				videos.appendChild(newVideoContainer);
+			}
+		}
+
+		rtcTrackEvent.track.onmute = () => {
+			delete peers[userID][source];
+			const videoToRemove = document.getElementsByClassName(`${userID} ${source}`);
+			if (videoToRemove) {
+				videoToRemove[0].parentElement.parentElement.removeChild(videoToRemove[0].parentElement);
+			}
+			if (Object.entries(peers[userID]).length === 0) {
+				delete peers[userID];
+			}
+		};
+	};
+};
+
+screenShareButton.addEventListener(
+	'click',
+	async () => {
+		const stream = await navigator.mediaDevices.getDisplayMedia({ cursor: true });
+		Object.keys(peers).forEach((userID) => createPeer(userID, 'screen'));
+		Object.values(peers).forEach((peer) => {
+			peer.screen.addTrack(stream.getVideoTracks()[0], stream);
+		});
+	},
+);
+
+(async () => {
+	const stream = await navigator
 		.mediaDevices
 		.getUserMedia({
 			audio: true,
 			video: true,
 		});
+
+	cameraVideo.srcObject = stream;
+
+	socket.connect();
+	socket.emit('join room', 'abc');
+
+	socket.on(
+		'another user disconnected',
+		(userID) => {
+			delete peers[userID];
+			const videosToRemove = document.getElementsByClassName(userID);
+			while (videosToRemove.length) {
+				videosToRemove[0].parentElement.parentElement.removeChild(videosToRemove[0].parentElement);
+			}
+		},
+	);
+
+	socket.on(
+		'another user joined',
+		(userID) => {
+			createPeer(userID, 'camera');
+			stream.getTracks().forEach((track) => peers[userID].camera.addTrack(track, stream));
+		},
+	);
+
+	socket.on(
+		'offer',
+		({
+			caller,
+			sdp,
+			source,
+		}) => {
+			createPeer(caller, source);
+			console.log(peers);
+			const description = new RTCSessionDescription(sdp);
+			peers[caller][source].setRemoteDescription(description)
+				.then(() => {
+					if (source === 'camera') {
+						stream.getTracks().forEach((track) => peers[caller][source].addTrack(track, stream));
+					}
+				})
+				.then(() => peers[caller][source].createAnswer())
+				.then((answer) => peers[caller][source].setLocalDescription(answer))
+				.then(() => {
+					socket.emit(
+						'answer',
+						{
+							sdp: peers[caller][source].localDescription,
+							target: caller,
+							source,
+						},
+					);
+				});
+		},
+	);
+		
+	socket.on(
+		'answer',
+		({
+			caller,
+			sdp,
+			source,
+		}) => {
+			peers[caller][source].setRemoteDescription(new RTCSessionDescription(sdp));
+		},
+	);
+
+	socket.on(
+		'ice-candidate',
+		({
+			candidate,
+			origin,
+			source,
+		}) => {
+			peers[origin][source].addIceCandidate(new RTCIceCandidate(candidate));
+		},
+	);
 })();
 
-socket.on(
-	'other user',
-	(userID) => {
-		peers[userID] = new RTCPeerConnection(servers);
-
-		peers[userID].onicecandidate = (rtcPeerConnectionIceEvent) => {
-			if (rtcPeerConnectionIceEvent.candidate) {
-				socket.emit('ice-candidate', {
-					candidate: rtcPeerConnectionIceEvent.candidate,
-					origin: socket.id,
-					target: userID,
-				});
-			}
-		};
-
-		peers[userID].ontrack = (rtcTrackEvent) => {
-			const existingVideo = document.getElementById(userID);
-			if (existingVideo) {
-				existingVideo.srcObject = rtcTrackEvent.streams[0];
-			} else {
-				document.getElementById('videos').innerHTML += `
-					<li>
-						<video autoplay id="${userID}" playsinline>
-						</video>
-					</li>
-				`;
-				document.getElementById(userID).srcObject = rtcTrackEvent.streams[0];
-			}
-		};
-
-		peers[userID].onnegotiationneeded = peers[userID].createOffer()
-			.then((offer) => peers[userID].setLocalDescription(offer))
-			.then(() => {
-				socket.emit(
-					'offer',
-					{
-						target: userID,
-						caller: socket.id,
-						sdp: peers[userID].localDescription,
-					},
-				);
-			})
-			.catch((error) => console.log(error));
-
-		localStream.getTracks().forEach((track) => peers[userID].addTrack(track, localStream));
-	},
-);
-
-socket.on(
-	'offer',
-	(incoming) => {
-		peers[incoming.caller] = new RTCPeerConnection(servers);
-		const description = new RTCSessionDescription(incoming.sdp);
-		peers[incoming.caller].setRemoteDescription(description)
-			.then(() => localStream.getTracks().forEach((track) => peers[incoming.caller].addTrack(track, localStream)))
-			.then(() => peers[incoming.caller].createAnswer())
-			.then((answer) => peers[incoming.caller].setLocalDescription(answer))
-			.then(() => {
-				socket.emit(
-					'answer',
-					{
-						target: incoming.caller,
-						caller: socket.id,
-						sdp: peers[incoming.caller].localDescription,
-					},
-				);
-			});
-	},
-);
-
-socket.on('answer', (answer) => peers[answer.caller].setRemoteDescription(new RTCSessionDescription(answer.sdp)));
-socket.on('ice-candidate', (incoming) => peers[incoming.origin].addIceCandidate(new RTCIceCandidate(incoming.candidate)));
-
-localVideo.srcObject = localStream;
--- src/index.ts
+++ src/index.ts
@@ -1,159 +1,125 @@
 import fastify_static from '@fastify/static';
 import Fastify from 'fastify';
 import socketioServer from 'fastify-socket.io';
-// import fastify_websocket from '@fastify/websocket';
 import path from 'path';
 
 const fastify = Fastify();
 
 fastify.register(socketioServer);
 
-// server.register(fastify_websocket);
-
 fastify.register(
 	fastify_static,
 	{	root: path.join(__dirname, '../public') },
 );
 
-const rooms = {};
-
-fastify.ready((err) => {
-	if (err) throw err;
-
+(async () => {
+	await fastify.ready();
 	fastify
 		.io
 		.on(
 			'connection',
 			(socket) => {
-					fastify
-						.io
-						.on(
-							'answer',
-							(payload) => {
-								fastify
-									.io
-									.to(payload.target)
-									.emit(
-										'answer',
-										payload,
-									);
-							},
-						);
+				socket
+					.on(
+						'answer',
+						({
+							sdp,
+							source,
+							target,
+						}) => {
+							fastify
+								.io
+								.to(target)
+								.emit(
+									'answer',
+									{
+										caller: socket.id,
+										source,
+										sdp,
+									},
+								);
+						},
+					);
 
-					fastify
-						.io
-						.on(
-							'ice-candidate',
-							(incoming) => {
-								fastify
-									.io
-									.to(incoming.target)
-									.emit(
-										'ice-candidate',
-										{
-											candidate: incoming.candidate,
-											origin: incoming.origin,
-										},
-									);
-							},
-						);
+				socket
+					.on(
+						'disconnecting',
+						() => {
+							socket
+								.rooms
+								.forEach(
+									(roomID) => {
+										socket
+										.to(roomID)
+										.emit(
+											'another user disconnected',
+											socket.id,
+										);
+									},
+								);
+						}
+					);
 
-					fastify
-						.io
-						.on(
-							'join room',
-							(roomID) => {
-								if (rooms[roomID]) {
-									rooms[roomID].push(socket.id);
-								} else {
-									rooms[roomID] = [socket.id];
-								}
+				socket
+					.on(
+						'ice-candidate',
+						({
+							candidate,
+							source,
+							target,
+						}) => {
+							fastify
+								.io
+								.to(target)
+								.emit(
+									'ice-candidate',
+									{
+										candidate,
+										origin: socket.id,
+										source,
+									},
+								);
+						},
+					);
 
-								const otherUser = rooms[roomID].find((socketID) => socketID !== socket.id);
+				socket
+					.on(
+						'join room',
+						(roomID) => {
+							socket.join(roomID);
+							socket
+								.to(roomID)
+								.emit(
+									'another user joined',
+									socket.id,
+								);
+						},
+					);
 
-								if (otherUser) {
-									socket.emit('other user', otherUser);
-									socket.to(otherUser).emit('user joined', socket.id);
-								}
-							},
-						);
-
-					fastify
-						.io
-						.on(
-							'offer',
-							(payload) => {
-								fastify
-									.io
-									.to(payload.target)
-									.emit(
-										'offer',
-										payload,
-									);
-							},
-						);
+				socket
+					.on(
+						'offer',
+						({
+							sdp,
+							source,
+							target,
+						}) => {
+							fastify
+								.io
+								.to(target)
+								.emit(
+									'offer',
+									{
+										caller: socket.id,
+										sdp,
+										source,
+									},
+								);
+						},
+					);
 			},
 		);
-});
-
-// server.register(async function (server) {
-// 	server.route({
-// 		handler: (req, reply) => {
-// 			reply.sendFile('index.html');
-// 		},
-// 		method: 'GET',
-// 		url: '/',
-// 		wsHandler: (connection, req) => {
-// 			// connection.setEncoding('utf8');
-// 			connection.socket.
-// 			broadcast({
-// 				sender: '__server',
-// 				message: `${(req.query as { username: string }).username} joined`,
-// 			});
-
-// 			connection.socket.on('close', () => {
-// 				broadcast({
-// 					sender: '__server',
-// 					message: `${(req.query as { username: string }).username} left`,
-// 				});
-// 			});
-
-// 			connection.socket.on('message', (message: string) => {
-// 				broadcast({
-// 					sender: (req.query as { username: string }).username,
-// 					...JSON.parse(message),
-// 				});
-// 			});
-
-// 			// connection.once('data', chunk => {
-// 			// 	connection.end();
-// 			// });
-// 		}
-// 	})
-// });
-
-// server.register(async function (server) {
-// 	server.get('/', { websocket: true }, (connection, req) => {
-// 		broadcast({
-// 			sender: '__server',
-// 			message: `${(req.query as { username: string }).username} joined`,
-// 		});
-
-// 		connection.socket.on('close', () => {
-// 			broadcast({
-// 				sender: '__server',
-// 				message: `${(req.query as { username: string }).username} left`,
-// 			});
-// 		});
-
-// 		connection.socket.on('message', (message: string) => {
-// 			broadcast({
-// 				sender: (req.query as { username: string }).username,
-// 				...JSON.parse(message),
-// 			});
-// 		});
-// 	});
-// });
+})();
 
 (async () => {
 	try {
@@ -164,9 +130,3 @@ fastify.ready((err) => {
 		process.exit(1);
 	}
 })();
-
-// function broadcast(message: object) {
-// 	for(let client of server.websocketServer.clients) {
-// 		client.send(JSON.stringify(message));
-// 	}
-// }
